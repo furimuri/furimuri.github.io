33358ccc545cac571dc4d2ec2ca47731e63873f1
unknown <jys922@gmail.com>
2021-09-26 21:24:34 +0800

    Changes to be committed: 	new file:   css/fancybox.css 	modified:   css/furimuri.css 	new file:   img/work/01.jpg 	new file:   img/work/02.jpg 	new file:   img/work/03.jpg 	new file:   img/work/04.jpg 	modified:   index.html 	new file:   js/fancybox.umd.js 	modified:   js/furimuri.js 	new file:   js/jquery-1.7.min.js Add work page. Add lightbox effect.

--- js/furimuri.js
+++ js/furimuri.js
@@ -8,3 +8,31 @@ $("#homePage").click(function(){
     let rightContent = $("#rightContent");
     rightContent.html("<img src='./img/home.png' class='home rounded float-left img-fluid'>");
 });
+
+$("#workPage").click(function(){
+    let rightContent = $("#rightContent");
+
+    let contentStr = "";
+    contentStr = contentStr.concat("<a href='./img/work/01.jpg' data-fancybox='gallery' data-caption='Optional caption'>");
+    contentStr = contentStr.concat("<img class='img-fluid' style='max-width: 200px;' src='./img/work/01.jpg' alt=''>");
+    contentStr = contentStr.concat("</a>");
+    contentStr = contentStr.concat("<a href='./img/work/02.jpg' data-fancybox='gallery' data-caption='Optional caption'>");
+    contentStr = contentStr.concat("<img class='img-fluid' style='max-width: 200px;' src='./img/work/02.jpg' alt=''>");
+    contentStr = contentStr.concat("</a>");
+    contentStr = contentStr.concat("<a href='./img/work/03.jpg' data-fancybox='gallery' data-caption='Optional caption'>");
+    contentStr = contentStr.concat("<img class='img-fluid' style='max-width: 200px;' src='./img/work/03.jpg' alt=''>");
+    contentStr = contentStr.concat("</a>");
+    console.log(contentStr);
+    rightContent.html(contentStr);
+
+});
+
+// if(jQuery().fancybox) {
+//     $(document).ready(function() {
+//         $(".fancybox-pic").fancybox();
+//     });
+// } 
+// $(document).ready(function() {
+//     $(".fancybox-pic").fancybox();
+// });
+
