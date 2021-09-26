5d7d3d897132dae929d3e825fb1228ccd3120c33
unknown <jys922@gmail.com>
2021-09-26 22:51:02 +0800

    Changes to be committed: 	modified:   css/furimuri.css 	modified:   index.html 	modified:   js/furimuri.js Add scroll bar always display. Add right content title.

--- js/furimuri.js
+++ js/furimuri.js
@@ -1,30 +1,51 @@
 $("a.nav-link").click(function(){
-    console.log("456");
+    let rightTitle = $("#rightTitle");
     $(this).siblings().removeClass("active");
     $(this).addClass("active");
+    rightTitle.html(this.name);
 });
 
 $("#homePage").click(function(){
+    let rightTitle = $("#rightTitle");
     let rightContent = $("#rightContent");
-    rightContent.html("<img src='./img/home.png' class='home rounded float-left img-fluid'>");
+    let contentStr = "";
+    contentStr = contentStr.concat("<div class='row'>");
+        contentStr = contentStr.concat("<div class='col-12'>");
+        contentStr = contentStr.concat("<img src='./img/home.png' class='home rounded float-left img-fluid'>");
+        contentStr = contentStr.concat("</a>");
+    contentStr = contentStr.concat("</div>");
+
+    rightContent.html(contentStr);
+    rightTitle.html("");
 });
 
 $("#workPage").click(function(){
     let rightContent = $("#rightContent");
 
     let contentStr = "";
-    contentStr = contentStr.concat("<a href='./img/work/01.jpg' data-fancybox='gallery' data-caption='Optional caption'>");
-    contentStr = contentStr.concat("<img class='img-fluid' style='max-width: 200px;' src='./img/work/01.jpg' alt=''>");
-    contentStr = contentStr.concat("</a>");
-    contentStr = contentStr.concat("<a href='./img/work/02.jpg' data-fancybox='gallery' data-caption='Optional caption'>");
-    contentStr = contentStr.concat("<img class='img-fluid' style='max-width: 200px;' src='./img/work/02.jpg' alt=''>");
-    contentStr = contentStr.concat("</a>");
-    contentStr = contentStr.concat("<a href='./img/work/03.jpg' data-fancybox='gallery' data-caption='Optional caption'>");
-    contentStr = contentStr.concat("<img class='img-fluid' style='max-width: 200px;' src='./img/work/03.jpg' alt=''>");
-    contentStr = contentStr.concat("</a>");
-    console.log(contentStr);
-    rightContent.html(contentStr);
+    contentStr = contentStr.concat("<div class='row'>");
+        contentStr = contentStr.concat("<div class='col-6'>");
+            contentStr = contentStr.concat("<a href='./img/work/01.jpg' data-fancybox='gallery' data-caption='Optional caption'>");
+            contentStr = contentStr.concat("<img class='work img-fluid' style='max-width: 300px;' src='./img/work/01.jpg' alt=''>");
+            contentStr = contentStr.concat("</a>");
+        contentStr = contentStr.concat("</div>");
+        contentStr = contentStr.concat("<div class='col-6'>");
+            contentStr = contentStr.concat("<a href='./img/work/02.jpg' data-fancybox='gallery' data-caption='Optional caption'>");
+            contentStr = contentStr.concat("<img class='work img-fluid' style='max-width: 300px;' src='./img/work/02.jpg' alt=''>");
+            contentStr = contentStr.concat("</a>");
+        contentStr = contentStr.concat("</div>");
+    contentStr = contentStr.concat("</div>");
+    contentStr = contentStr.concat("<div class='row'>");
+        contentStr = contentStr.concat("<div class='col-6'>");
+            contentStr = contentStr.concat("<a href='./img/work/03.jpg' data-fancybox='gallery' data-caption='Optional caption'>");
+            contentStr = contentStr.concat("<img class='work img-fluid' style='max-width: 300px;' src='./img/work/03.jpg' alt=''>");
+            contentStr = contentStr.concat("</a>");
+        contentStr = contentStr.concat("</div>");
+        contentStr = contentStr.concat("<div class='col-6'>");
+        contentStr = contentStr.concat("</div>");
+    contentStr = contentStr.concat("</div>");
 
+    rightContent.html(contentStr);
 });
 
 // if(jQuery().fancybox) {
