34cc957a303ca87b3f3f49292328eed71840f80e
unknown <jys922@gmail.com>
2021-09-26 20:07:53 +0800

    Changes to be committed: 	modified:   css/furimuri.css 	new file:   img/home.png 	modified:   index.html 	modified:   js/furimuri.js Home page.

--- js/furimuri.js
+++ js/furimuri.js
@@ -1,6 +1,10 @@
-console.log("123");
 $("a.nav-link").click(function(){
     console.log("456");
     $(this).siblings().removeClass("active");
     $(this).addClass("active");
+});
+
+$("#homePage").click(function(){
+    let rightContent = $("#rightContent");
+    rightContent.html("<img src='./img/home.png' class='home rounded float-left img-fluid'>");
 });
