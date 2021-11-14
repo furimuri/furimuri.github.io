28b5a0c2c3d47cb8d039d6f793250b39ebe68147
unknown <jys922@gmail.com>
2021-11-14 20:09:26 +0800

    Changes to be committed: 	modified:   css/furimuri.css 	modified:   index.html 	modified:   js/furimuri.js 	modified:   json/images.json Add rounded style to images. Fix home page image responsive.

--- js/furimuri.js
+++ js/furimuri.js
@@ -95,7 +95,7 @@ $("a[name='classPage']").click(function($this){
         }
         contentStr = contentStr.concat("<div class='col-lg-4 col-md-12 col-sm-12'>");
         contentStr = contentStr.concat("<a href='./img/"+ myFuriWorks[thisID].folder +"/" + myFuriWorks[thisID].images[i].name + ".jpg" + "' data-fancybox='gallery' data-caption='<h4>"+ imgTitle + "</h4><br><h5>" + imgContent +"</h5>'>");
-        contentStr = contentStr.concat("<img class='work img-fluid' style='max-width: 300px;' src='./img/"+myFuriWorks[thisID].folder+"/" + myFuriWorks[thisID].images[i].name + ".jpg" + "' alt=''>");
+        contentStr = contentStr.concat("<img class='work img-fluid rounded' style='max-width: 300px;' src='./img/"+myFuriWorks[thisID].folder+"/" + myFuriWorks[thisID].images[i].name + ".jpg" + "' alt=''>");
         contentStr = contentStr.concat("</a>");
         contentStr = contentStr.concat("</div>");
         if((i+1)%3==0 && i!=0) {
